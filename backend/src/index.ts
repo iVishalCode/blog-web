62ba1aaeecb51d17da5e753d7f9936c5f5314ae6
1VishalSD <ilearnvk@gmail.com>
2024-11-22 11:30:05 +0530

    make an api with hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,31 @@
 import { Hono } from "hono";
-
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 const app = new Hono();
 
-app.post("/api/v1/user/signup", (c) => c.text("SignUp"));
-app.post("/api/v1/user/signin", (c) => c.text("Signin"));
-app.put("/api/v1/blog", (c) => c.text("Blog"));
-app.get("/api/v1/blog/:id", (c) => c.text("Blog -Id"));
+app.post("/api/v1/user/signup", (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  //@ts-ignore
+  const body = await c.req.json();
+  //@ts-ignore
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  return c.text("SignUp");
+});
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Signin");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text("Blog");
+});
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Blog -Id");
+});
 export default app;

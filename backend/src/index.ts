d462b107611fcd803da3ebc5fd13b998f4cc4524
1VishalSD <ilearnvk@gmail.com>
2024-11-26 09:42:53 +0530

    write the backend for medium

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,9 @@
-import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-const app = new Hono();
+import { Hono } from 'hono'
 
-app.post("/api/v1/user/signup", (c) => {
-  const prisma = new PrismaClient({
-    //@ts-ignore
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  //@ts-ignore
-  const body = await c.req.json();
-  //@ts-ignore
-  await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-    },
-  });
-  return c.text("SignUp");
-});
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Signin");
-});
-app.put("/api/v1/blog", (c) => {
-  return c.text("Blog");
-});
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("Blog -Id");
-});
-export default app;
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app

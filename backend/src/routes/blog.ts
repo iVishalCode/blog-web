fffe3bc5437b3175b3a81e67480098b42184ddb6
1VishalSD <ilearnvk@gmail.com>
2024-11-28 12:06:02 +0530

    write the blog logic

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,81 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { Bindings } from "hono/types";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+blogRouter.use("/*", async (c, next) => {
+  const token = c.req.header("Authorization");
+  next();
+});
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blog = await prisma.blog.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: 1,
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blog = await prisma.blog.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+blogRouter.get("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const blog = await prisma.blog.findFirst({
+      where: {
+        id: body.id,
+      },
+    });
+    return c.json({
+      id: blog.id,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.json({
+      message: "Error while fetching the blog",
+    });
+  }
+});
+blogRouter.get("/bulk", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blogs = await prisma.blog.findMany();
+  return c.json({
+    blogs,
+  });
+});
